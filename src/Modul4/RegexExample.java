c36bc11621b78b03aecf984917c30c5e6ae136f9
Alexandra <alexandra.dolgova95@gmail.com>
2021-09-25 15:19:51 +0300

    All done

--- src/Modul4/RegexExample.java
+++ src/Modul4/RegexExample.java
@@ -11,10 +11,7 @@ import java.util.regex.Pattern;
             Matcher matcher = pattern.matcher(str);
             while (matcher.find()) {
                 System.out.println("Email option: " + matcher.group());
-                System.out.println("Email option: " + matcher.group());
-                System.out.println("Email option: " + matcher.group());
-                System.out.println("Email option: " + matcher.group());
-                System.out.println("Email option: " + matcher.group());
+                System.out.println("branch");
             }
         }
     }
